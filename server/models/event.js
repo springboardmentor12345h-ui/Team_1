b2c2d50b774c392e0469964b0c7c188bbcf24ca7
sravanthi141005 <sravanthi141005@gmail.com>
2026-01-02 23:08:43 +0530

    update

--- server/models/event.js
+++ server/models/event.js
@@ -19,7 +19,7 @@ const eventSchema = new mongoose.Schema({
   createdBy: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
-    required: true
+    required: false
   },
 
   created_at: {
